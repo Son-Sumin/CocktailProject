3e046bd481fb4424813e7a0282d016fafb7c62e0
Son-Sumin <goldkitty007@gmail.com>
2023-02-22 17:23:01 +0900

    ERD 수정

--- cocktail/src/main/java/com/bitacademy/cocktail/domain/Signature.java
+++ cocktail/src/main/java/com/bitacademy/cocktail/domain/Signature.java
@@ -6,12 +6,10 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
@@ -53,6 +51,7 @@ public class Signature extends BaseTimeEntity {
 	
 	
 	@ToString.Exclude
+	@Builder.Default
 	@OneToMany(mappedBy = "signature", cascade = CascadeType.ALL)
 	@JsonIgnoreProperties({"signature"})
 	private List<ReviewSignature> reviewSignatures = new ArrayList<>();
