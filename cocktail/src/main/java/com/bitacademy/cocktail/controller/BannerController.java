1add33672790dc708b08c76d285c25d8f7ebaa47
Son-Sumin <goldkitty007@gmail.com>
2023-03-02 16:12:31 +0900

    이미지 변환 중

--- cocktail/src/main/java/com/bitacademy/cocktail/controller/BannerController.java
+++ cocktail/src/main/java/com/bitacademy/cocktail/controller/BannerController.java
@@ -1,11 +1,18 @@
 package com.bitacademy.cocktail.controller;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import java.util.UUID;
 
 import javax.transaction.Transactional;
 
+import org.apache.commons.io.IOUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -33,35 +40,69 @@ public class BannerController {
 	private final BannerRepository bannerRepository;
 	
 	
+//	/* 배너 리스트 */
+//	@GetMapping({"", "/list"})
+//	public List<Banner> listBanner(Model model) {
+//		List<Banner> banner = bannerRepository.findAll();
+//		model.addAttribute("banners", banner);
+//		return banner;
+//	}
+	
 	/* 배너 리스트 */
 	@GetMapping({"", "/list"})
-	public List<Banner> listBanner(Model model) {
+	public void listBanner(Model model, String filename) {
 		List<Banner> banner = bannerRepository.findAll();
 		model.addAttribute("banners", banner);
-		return banner;
+		
+		//showImage(filename);
+		//return listBanner(filename);
+	}
+	
+	@GetMapping(value = {"/{filename}", "/list/{filename}"}, produces = {MediaType.IMAGE_GIF_VALUE, MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE,})
+	public ResponseEntity<byte[]> showImages(@PathVariable("filename") String filename) throws IOException {
+		
+		byte[] res = bannerRepository.findByFilename(filename);
+		System.out.println("filename : " + filename);
+		System.out.println("res : " + res);
+	    return new ResponseEntity<>(res, HttpStatus.OK);
+		
+//		InputStream imageStream = new FileInputStream("C://images/feed/" + no);
+//		byte[] imageByteArray = IOUtils.toByteArray(imageStream);
+//		imageStream.close();
+//		return new ResponseEntity<byte[]>(imageByteArray, HttpStatus.OK);
 	}
 	
 	/* 배너 추가 */
 	@CrossOrigin(origins = "*")
 	@PostMapping("/add")
 	public void addBanner(@ModelAttribute Banner form, MultipartFile file) throws Exception {
-
-		// 프로젝트 경로 설정, 랜덤한 문자열이 들어간 파일이름 설정
-		String projectPath = System.getProperty("user.dir") + "\\src\\main\\resources\\static\\files";
-		UUID uuid = UUID.randomUUID();
-		String fileName = uuid + "_" + file.getOriginalFilename();
 		
-		// MultipartFile file 넣어줄 껍데기 지정 (경로, "파일이름")
-		File saveFile = new File(projectPath, fileName);
-		file.transferTo(saveFile);
-		
-		// 사진 추가		
 		Banner banner = new Banner();
-		banner.setTitle(form.getTitle());
-		banner.setFilename(file.getOriginalFilename());
-		banner.setFilepath("/files/" + fileName);
+		
+		 // 파일을 올리지 않을 경우
+		 if(file.isEmpty()) {
+			banner.setTitle(form.getTitle());
+			banner.setFilename("");
+			banner.setFilepath("");
+			bannerRepository.save(banner);
+			 
+	     } else {
+	    	// 프로젝트 경로 설정, 랜덤한 문자열이 들어간 파일이름 설정
+	 		String projectPath = System.getProperty("user.dir") + "\\src\\main\\resources\\static\\files";
+	 		UUID uuid = UUID.randomUUID();
+	 		String fileName = uuid + "_" + file.getOriginalFilename();
+	 		
+	 		// MultipartFile file 넣어줄 껍데기 지정 (경로, "파일이름")
+	 		File saveFile = new File(projectPath, fileName);
+	 		file.transferTo(saveFile);
+			
+			// 사진 추가
+			banner.setTitle(form.getTitle());
+			banner.setFilename(file.getOriginalFilename());
+			banner.setFilepath("/files/" + fileName);
 
-		bannerRepository.save(banner);
+			bannerRepository.save(banner);
+	     }
 	}
 	
 	/* 배너 삭제 */
@@ -82,26 +123,32 @@ public class BannerController {
 		banner = bannerRepository.findByNo(no);
 		
 		// 기존에 올린 파일 있으면 지우기
-		if(file != null){
+		if(file.isEmpty()) {
 			banner.setFilename("");
 			banner.setFilepath("");
         }
 		
-		// 프로젝트 경로 설정, 랜덤한 문자열이 들어간 파일이름 설정
-		String projectPath = System.getProperty("user.dir") + "\\src\\main\\resources\\static\\files";
-		UUID uuid = UUID.randomUUID();
-		String fileName = uuid + "_" + file.getOriginalFilename();
-		
-		// MultipartFile file 넣어줄 껍데기 지정 (경로, "파일이름")
-		File saveFile = new File(projectPath, fileName);
-		file.transferTo(saveFile);
-		
-		// 수정
-		banner.setTitle(form.getTitle());
-		banner.setFilename(file.getOriginalFilename());
-		banner.setFilepath("/files/" + fileName);
-		
-		bannerRepository.save(banner);
-		
+		if(file.isEmpty()) {
+			banner.setTitle(form.getTitle());
+			banner.setFilename("");
+			banner.setFilepath("");
+			bannerRepository.save(banner);
+			
+	    } else {
+	    	// 프로젝트 경로 설정, 랜덤한 문자열이 들어간 파일이름 설정
+			String projectPath = System.getProperty("user.dir") + "\\src\\main\\resources\\static\\files";
+			UUID uuid = UUID.randomUUID();
+			String fileName = uuid + "_" + file.getOriginalFilename();
+			
+			// MultipartFile file 넣어줄 껍데기 지정 (경로, "파일이름")
+			File saveFile = new File(projectPath, fileName);
+			file.transferTo(saveFile);
+			
+			// 수정
+			banner.setTitle(form.getTitle());
+			banner.setFilename(file.getOriginalFilename());
+			banner.setFilepath("/files/" + fileName);
+			bannerRepository.save(banner);
+	    }
 	}
 }
