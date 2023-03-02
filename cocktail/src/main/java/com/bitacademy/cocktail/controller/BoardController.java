4c9465486aa4bf2b2ae3fb58635a81be391b4fca
Son-Sumin <goldkitty007@gmail.com>
2023-03-02 09:12:02 +0900

    test

--- cocktail/src/main/java/com/bitacademy/cocktail/controller/BoardController.java
+++ cocktail/src/main/java/com/bitacademy/cocktail/controller/BoardController.java
@@ -45,7 +45,7 @@ public class BoardController {
 //	게시글 작성
 	@PostMapping("/board/write")
 	public void boardWrite(@ModelAttribute Board board, BoardImage boardImage, List<MultipartFile> files) throws Exception {
-		
+		System.out.println("board = " + board);
 		board.setHit(0L);
 		boardService.boardWrite(board);
 		
