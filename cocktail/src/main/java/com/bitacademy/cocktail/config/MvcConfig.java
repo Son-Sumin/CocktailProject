78888349dbdd64e1eef1d6dbb9cca71e5c7aa32b
Son-Sumin <goldkitty007@gmail.com>
2023-03-10 21:26:07 +0900

    cocktail project

--- cocktail/src/main/java/com/bitacademy/cocktail/config/MvcConfig.java
+++ cocktail/src/main/java/com/bitacademy/cocktail/config/MvcConfig.java
@@ -9,11 +9,11 @@
 //@Configuration
 //public class MvcConfig {
 //
-//	final Path fileRoot  = Paths.get("./public").toAbsolutePath().normalize();
+//	final Path fileRoot = Paths.get("./public").toAbsolutePath().normalize();
 //
 //    public void addResourceHandlers(ResourceHandlerRegistry registry) {
 //    	registry.addResourceHandler("/images/**")
-//        	.addResourceLocations("classpath:/static/images/");
+//        	.addResourceLocations("classpath:/static/");
 //		registry.addResourceHandler("/files/**")
 //		        .addResourceLocations("file:/path/to/dynamic/directory/");
 //    	
--- cocktail/src/main/java/com/bitacademy/cocktail/domain/Ingredient.java
+++ cocktail/src/main/java/com/bitacademy/cocktail/domain/Ingredient.java
@@ -50,7 +50,7 @@ public class Ingredient {
 	private List<CocktailRecipe> cocktailRecipes = new ArrayList<>();
 	
 	@JsonIgnore
-	//@JsonIgnoreProperties({"signatureRecipes"})
+	//@JsonIgnoreProperties({"ingredient"})
 	@OneToMany(mappedBy = "ingredient", cascade = CascadeType.ALL)
 	private List<SignatureRecipe> signatureRecipes = new ArrayList<>();
 	
